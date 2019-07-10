0a7910334ba47ad0f0bbf90b6d95c78b7ae40d9d
Alexander Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2019-07-09 19:32:38 -0600

    Initial commit.

---
+++ src/index.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { onMount } from "svelte";
+
+  let observerCallback = function(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        intersected = true;
+        observer.unobserve(imgElement);
+      }
+    });
+  };
+
+  export let placeholder;
+  export let src;
+  export let alt;
+
+  let imgElement;
+  let path;
+
+  let observer = new IntersectionObserver(observerCallback);
+  let intersected = false;
+  let loaded = false;
+
+  $: {
+    path = intersected ? src : placeholder;
+  }
+
+  onMount(() => {
+    observer.observe(imgElement);
+
+    return () => {
+      observer.unobserve(imgElement);
+    };
+  });
+
+  function handleLoad() {
+    if (!loaded && path === src) {
+      loaded = true;
+    }
+  }
+</script>
+
+<img
+  src={path}
+  alt={alt}
+  on:load={handleLoad}
+  bind:this={imgElement}
+  class="svelte-lazy-image"
+  class:svelte-lazy-image--loaded="{loaded}"
+  {...$$props}
+/>
