6a556042bd31041a6a3a603eea52bd17d34bce0d
Alexander Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2022-01-18 18:13:58 -0700

    feat: action

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
+		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+	},
 };
 
 export default config;
